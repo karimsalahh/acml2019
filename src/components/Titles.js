3e1c7536178f50972d2d874a7e9d73668c84d159
ahmedshaker07 <ahmed_hossam_012@yahoo.com>
2019-12-09 19:01:20 +0200

    titles component added

---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Titles = props => (
+  <div>
+    <h1 className="title-container__title"> Music4U</h1>
+    <p className="title-container__subtitle">
+      {/* Get songs mathcing with your current weather on spotify. */}
+    </p>
+  </div>
+);
+export default Titles;
